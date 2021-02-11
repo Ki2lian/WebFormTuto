205c99efb702b16e9803602cb461b52748537fef
DESKTOP-26PBIHI\Killi <kignon12@gmail.com>
2021-02-11 15:14:47 +0100

    Ajout de la connexion SQL Server

--- WebFormTuto/Demo.aspx.cs
+++ WebFormTuto/Demo.aspx.cs
@@ -13,10 +13,21 @@ namespace WebFormTuto
         public object date;
         protected void Page_Load(object sender, EventArgs e)
         {
-
             string connetionString;
             SqlConnection cnn;
-            connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Killi\source\repos\WebFormTuto\WebFormTuto\App_Data\Database1.mdf;Integrated Security=True";
+            connetionString = @"Server=localhost\SQLEXPRESS;Trusted_Connection=True;Database=school ;User ID=user1;Password=123456789";
+
+            cnn = new SqlConnection(connetionString);
+
+            cnn.Open();
+            Response.Write("Connexion réussie");
+            cnn.Close();
+
+            /*
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Server=localhost\SQLEXPRESS;Trusted_Connection=True;Database=school ;User ID=user1;Password=123456789";
+            //connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Killi\source\repos\WebFormTuto\WebFormTuto\App_Data\Database1.mdf;Integrated Security=True";
 
             cnn = new SqlConnection(connetionString);
 
@@ -32,24 +43,24 @@ namespace WebFormTuto
             while (dataReader.Read())
             {
                 Output = Output + dataReader.GetValue(0) + " " + dataReader.GetValue(1) + "</br>";
-            }
+            }*/
 
-            Response.Write(Output);
-            dataReader.Close();
-            
-            SqlDataAdapter adapter = new SqlDataAdapter();
+            //Response.Write(Output);
+            //dataReader.Close();
+
+            //SqlDataAdapter adapter = new SqlDataAdapter();
             /* Insertion / Update */
-                //sql = "Insert into compte(nom, prenom) values('Martin', 'Pierre')";
-                //sql = "Update compte set nom='Martin' where nom='Correc'";
-                //adapter.InsertCommand = new SqlCommand(sql, cnn);
-                //adapter.InsertCommand.ExecuteNonQuery();
-            
+            //sql = "Insert into compte(nom, prenom) values('Martin', 'Pierre')";
+            //sql = "Update compte set nom='Martin' where nom='Correc'";
+            //adapter.InsertCommand = new SqlCommand(sql, cnn);
+            //adapter.InsertCommand.ExecuteNonQuery();
+
             /* Pour supprimer un élément d'une table */
-                //sql = "Delete compte where Id=3";
-                adapter.DeleteCommand = new SqlCommand(sql,cnn); 
-		        adapter.DeleteCommand.ExecuteNonQuery();
-            command.Dispose();
-            cnn.Close();
+            //sql = "Delete compte where Id=3";
+            //adapter.DeleteCommand = new SqlCommand(sql,cnn); 
+            //adapter.DeleteCommand.ExecuteNonQuery();
+            //command.Dispose();
+            //cnn.Close();
 
             this.date = DateTime.Now;
             Response.Write(Session["Name"]);
@@ -58,10 +69,10 @@ namespace WebFormTuto
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            Session["Name"] = txtName.Text;
+            //Session["Name"] = txtName.Text;
             
 
-            //Response.Write(txtName.Text + "</br>");
+            Response.Write(txtName.Text + "</br>");
 
             //Response.Write(lstLocation.SelectedItem.Text + "</br>");
 
--- WebFormTuto/TestControl.ascx
+++ WebFormTuto/TestControl.ascx
@@ -4,12 +4,3 @@
 <script runat="server">
 	  public int MinValue = 0;
 	</script>
-<table>
-	<tr>
-	  <td>Killian Tutorials</td>
-	</tr>
-	
-	<tr>
-	  <td> This Tutorial is for</td>
-	</tr>
-</table>
